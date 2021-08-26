4799a2b45848f296adfc70406951eb8cb98e4959
hao <hao.lee0019@gmail.com>
2021-08-26 12:12:47 +0100

    added sensor extrusion geometry

--- sentinel_model/solver.sh
+++ sentinel_model/solver.sh
@@ -6,14 +6,14 @@ restore0Dir
 rm -r log.decomposePar
 runApplication decomposePar -force
 
-#cp -f system/fvSchemes.dpm system/fvSchemes
-#cp -f system/fvSolution.dpm system/fvSolution
-#cp -f constant/kinematicCloudProperties.dpm constant/kinematicCloudProperties
-#rm -r log.DPMFoam
-#runParallel DPMFoam
-
 cp -f system/fvSchemes.mppic system/fvSchemes
 cp -f system/fvSolution.mppic system/fvSolution
 cp -f constant/kinematicCloudProperties.mppic constant/kinematicCloudProperties
 rm -r log.MPPICFoam
 runParallel MPPICFoam
+
+#cp -f system/fvSchemes.dpm system/fvSchemes
+#cp -f system/fvSolution.dpm system/fvSolution
+#cp -f constant/kinematicCloudProperties.dpm constant/kinematicCloudProperties
+#rm -r log.DPMFoam
+#runParallel DPMFoam
