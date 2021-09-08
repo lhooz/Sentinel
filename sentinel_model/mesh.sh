7f152a4c93e780db78b683475ff1712ca48a9b4e
hao <hao.lee0019@gmail.com>
2021-09-08 02:42:24 +0100

    increased particle size  5times

--- sentinel_model/mesh.sh
+++ sentinel_model/mesh.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 . ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions
 
+cd constant/triSurface
+
+surfaceTransformPoints -rollPitchYaw '(90 0 0)' sentinel_camera.stl camera.stl
+
+surfaceTransformPoints -translate '(0 0.134791 -0.057002)' camera.stl camera.stl
+
+surfaceTransformPoints -translate '(0 0.002 0)' sensorPad.stl sensor.stl
+
+cd ../..
+
 foamCleanTutorials
 
 runApplication blockMesh
@@ -9,6 +19,11 @@ runApplication surfaceFeatureExtract
 
 decomposePar
 
+cp -f system/snappyHexMeshDict.csnap system/snappyHexMeshDict
+runParallel snappyHexMesh -overwrite
+
+mv log.snappyHexMesh log.snappyHexMesh.csnap
+cp -f system/snappyHexMeshDict.addlayers system/snappyHexMeshDict
 runParallel snappyHexMesh -overwrite
 
 runApplication reconstructParMesh -constant -latestTime -mergeTol 1e-6
--- sentinel_model/solver.sh
+++ sentinel_model/solver.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 . ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions
 
+cp -f constant/turbulenceProperties.air.laminar constant/turbulenceProperties.air
+#cp -f constant/turbulenceProperties.air.LES constant/turbulenceProperties.air       
+
 restore0Dir
 
 rm -r log.decomposePar
